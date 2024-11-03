54316ed91a845187537ca65b2b1bfcddb92a7b9b
Sabyasachi Mitra <sabyasachihyd2003@gmail.com>
2024-11-03 20:39:14 +0530

    powershell 03-11-24 commit

--- PSAlias.ps1
+++ PSAlias.ps1
@@ -5,3 +5,15 @@
 # An alias in PowerShell is an alternate name for a command. A command may
 # have more than one alias. Unlike languages like Bash, an alias cannot include
 # parameters.
+# Get all alias
+Get-Alias
+# Get-Alias may be used to find the command behind an alias
+Get-Alias dir
+# Get-Alias may also be used to find the aliases for any command
+Get-Alias -Definition Get-ChildItem
+# Creating new alias
+New-Alias grep -Value Select-String
+# Removing Alias
+Remove-Alias grep
+# Removing Alias using Remove-Item
+Remove-Item Alias:\grep
---
+++ PSParams.ps1
@@ -0,0 +1,19 @@
+# This lab will illustrate Powershell Parameters
+# Date: 25-10-2024
+# Author: Sabyasachi Mitra
+#
+# Powershell supports four kinds of parameters. They can be identified in
+# Get-Help command description.
+#
+# Optional parameters, where parameter names and arguments are enclosed in a single pair
+# of square brackets.
+#
+# Optional positional parameters – the same as an optional parameter but with the parameter
+# name also enclosed in square brackets.
+#
+# Mandatory parameters, where the parameter name and argument are not bracketed.
+#
+# Mandatory positional parameters, where the parameter name is in square brackets,
+# but the argument is not.
+#
+# Optional Parameters :: 
